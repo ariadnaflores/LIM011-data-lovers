a214069edd9dbfa81ae33c4cb7a21b8ad78e2af4
ariadnaflores <ariadnafloresvilchez@gmail.com>
2019-10-28 09:27:24 -0500

    actualizando datos

--- src/main.js
+++ src/main.js
@@ -6,18 +6,8 @@ start.addEventListener('click',() =>{
     secondView.classList.remove('hide');
 }
 );
-/**
- * import POKEMON from './data/pokemon/pokemon.js'
- * import LoL from './data/lol/lol.js'
- * import POTTER from './data/potter/potter.js'
- */
+import POKEMON from './data/pokemon/pokemon.js';
+console.log(POKEMON);
 
-/*import { example } from './data.js';
-
-console.log(example);*/
-
-/*
- * console.log(POKEMON);
- * console.log(LoL);
- * console.log(POTTER)
-*/
+import { example } from './data.js';
+console.log(example);
